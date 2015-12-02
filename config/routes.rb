9f54a575a792aad28dac59ef2d5b39b060be8260
smurfik <smurfik@mlpinit.com>
2015-12-01 20:29:06 -0800

    add link to root path for logo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/', to: 'products#index'
+  get '/', to: 'products#index', as: :root
   get '/products/:id', to: 'products#show', as: :product
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
