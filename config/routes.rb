5021d41a59bdf86e5c62fa9e3856f27ca967cae9
smurfik <smurfik@mlpinit.com>
2015-12-01 16:02:45 -0800

    add product/:id route and show method & view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get '/', to: 'products#index'
+  get '/products/:id', to: 'products#show', as: :product
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
