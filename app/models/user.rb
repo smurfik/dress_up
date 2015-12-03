f8cf02251a72df3a1123ac0ceee652c88b5a9b22
smurfik <smurfik@mlpinit.com>
2015-12-02 16:57:21 -0800

    add css to error messages/ update validation for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :email, :password_digest, presence: true
+  validates :email, presence: true
   validates :email, uniqueness: true
+
   has_many :orders
 end
