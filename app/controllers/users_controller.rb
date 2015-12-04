00e04e1e6f134b4b2ce9a8e0a975f6465182246f
smurfik <smurfik@mlpinit.com>
2015-12-04 11:09:46 -0800

    add additional flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user &&  @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to root_path, notice: "Welcome!"
     else
       redirect_to sign_in_path, notice: "Wrong username or password. Try again!"
     end
@@ -24,14 +24,14 @@ class UsersController < ApplicationController
 
   def sign_out
     session.delete(:user_id)
-    redirect_to root_path
+    redirect_to root_path, notice: "See you soon!"
   end
 
   def create
     @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation))
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to root_path, notice: "Welcome!"
     else
       render :sign_up
     end
