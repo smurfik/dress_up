7e3a763097cbc93b35ed1845b8f873c1b8dc69f7
smurfik <smurfik@mlpinit.com>
2015-12-08 11:34:44 -0800

    add conditional to validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,15 @@
 class Order < ActiveRecord::Base
 
-  validates :name, presence: true
-  validates :shipping_address, presence: true
-  validates :total, presence: true
+  validates :name, presence: true, if: :status_complete?
+  validates :shipping_address, presence: true, if: :status_complete?
+  validates :total, presence: true, if: :status_complete?
 
   has_many :items, dependent: :destroy
   belongs_to :user
+
+  def status_complete?
+    self.status == "complete"
+  end
 
   def total_price
     total_price = 0
