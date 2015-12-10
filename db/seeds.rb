5c0445864d9b71138d07b67a079d5b2e34789dfe
smurfik <smurfik@mlpinit.com>
2015-12-09 22:21:30 -0800

    add admin user to sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,5 @@ until Product.count >= 10
   product = Product.create(image_url: "http://placekitten.com/40#{rand(10)}/40#{rand(10)}", name: Faker::Commerce.product_name, description: Faker::Lorem.paragraph(2))
   product.product_options.create(name: Faker::Commerce.color, price_in_cents: (rand(100) + 5) * 100)
 end
+
+User.create(email: "admin@example.com", password: "gogo", password_confirmation: "gogo", admin: true)
